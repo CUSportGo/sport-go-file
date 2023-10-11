bfce9fb7681c5d3cc0a5c61b54c9c9fab703d217
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-11 15:53:06 +0700

    create getSignedURL function

--- src/client/gcs.client.ts
+++ src/client/gcs.client.ts
@@ -1,10 +1,12 @@
-import { Bucket, Storage } from '@google-cloud/storage';
+import { Bucket, GetSignedUrlConfig, Storage } from '@google-cloud/storage';
+import { status } from '@grpc/grpc-js';
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { Stream } from 'stream';
 import storageConfig from '../config/google-cloud-config';
 
 @Injectable()
-export class GcsClient {
+export class GCSClient {
   private storage: Storage;
   private bucketName: string;
   private bucket: Bucket;
@@ -31,10 +33,32 @@ export class GcsClient {
     try {
       passthroughStream.pipe(file.createWriteStream()).on('finish', () => {
         console.log('finish');
-        // save on db
       });
     } catch (error) {
       console.log(error);
+      throw new RpcException({
+        code: status.INTERNAL,
+        message: 'Internal server error',
+      });
+    }
+  }
+
+  async getSignedURL(filename: string) {
+    try {
+      const options: GetSignedUrlConfig = {
+        version: 'v2',
+        action: 'read',
+        expires: Date.now() + 1000 * 60 * 15,
+      };
+
+      const file = this.bucket.file(filename);
+      const [url] = await file.getSignedUrl(options);
+      return url;
+    } catch (error) {
+      throw new RpcException({
+        code: status.INTERNAL,
+        message: 'Internal server error',
+      });
     }
   }
 }
