d8e6380aefc0e83a1f2045be4f6fb1e5ea3f6634
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-11 14:40:12 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FileModule],
+  imports: [FileModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
