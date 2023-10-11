85afa35c90484fb83c02ac2287c2849eab07ced7
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-11 14:26:08 +0700

    expose microservices connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const fileGprcOption: MicroserviceOptions = {
+    transport: Transport.GRPC,
+    options: {
+      package: 'file',
+      protoPath: join(__dirname, 'proto/file.proto'),
+      url: 'localhost:8086',
+    },
+  };
+  app.connectMicroservice(fileGprcOption);
+
+  await app.startAllMicroservices();
 }
 bootstrap();
