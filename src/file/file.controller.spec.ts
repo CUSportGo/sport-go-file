8b812fe2f6e4270db69cddafd6558de624311d89
NitiwatOwen <nitiwat3478@gmail.com>
2023-11-09 20:38:02 +0700

    fix failed test

--- src/file/file.controller.spec.ts
+++ src/file/file.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FileController } from './file.controller';
+import { FileService } from './file.service';
 
 describe('FileController', () => {
   let controller: FileController;
 
+  const mockFileService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FileController],
+      providers: [
+        {
+          provide: FileService,
+          useValue: mockFileService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FileController>(FileController);
--- src/file/file.service.spec.ts
+++ src/file/file.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { GCSClient } from '../client/gcs.client';
+import { FileRepository } from '../repository/file.repository';
 import { FileService } from './file.service';
 
 describe('FileService', () => {
   let service: FileService;
 
+  const mockFileRepo = {};
+  const mockGCSClient = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FileService],
+      providers: [
+        FileService,
+        {
+          provide: FileRepository,
+          useValue: mockFileRepo,
+        },
+        {
+          provide: GCSClient,
+          useValue: mockGCSClient,
+        },
+      ],
     }).compile();
 
     service = module.get<FileService>(FileService);
